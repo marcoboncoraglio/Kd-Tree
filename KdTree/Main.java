095e4340a0dee79b197b3af12706937185b211b5
Marco <marcoboncoraglio@gmail.com>
2018-05-25 19:27:08 +0200

    Added test dataset

--- KdTree/Main.java
+++ KdTree/Main.java
@@ -7,7 +7,7 @@ package KdTree;
 public class Main {
 
     public static void main(String[] args) {
-        double[][] dataset = Util.Util.readCSV("C:\\Users\\Marco\\Desktop\\Madison.csv", ",");
+        double[][] dataset = Util.Util.readCSV("C:\\Users\\Marco\\Documents\\NetBeansProjects\\mavenproject1\\Kd-Tree\\src\\main\\java\\Dataset\\Madison.csv", ",");
         KdTree.Node root = new Node(dataset);
     }
 }
